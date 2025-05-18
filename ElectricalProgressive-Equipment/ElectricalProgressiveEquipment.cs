7eb05217c000e3eda0b5adce87a2111740ff6c11
tehtelev <tehtelev@gmail.com>
2025-05-18 15:46:41 +0300

    Обновление 1.0.1

--- ElectricalProgressive-Equipment/ElectricalProgressiveEquipment.cs
+++ ElectricalProgressive-Equipment/ElectricalProgressiveEquipment.cs
@@ -9,15 +9,15 @@ using Vintagestory.API.Common.Entities;
 
 
 [assembly: ModDependency("game", "1.20.0")]
-[assembly: ModDependency("electricalprogressivecore", "1.0.0")]
-[assembly: ModDependency("electricalprogressivebasics", "1.0.0")]
-[assembly: ModDependency("electricalprogressiveqol", "1.0.0")]
+[assembly: ModDependency("electricalprogressivecore", "1.0.1")]
+[assembly: ModDependency("electricalprogressivebasics", "1.0.1")]
+[assembly: ModDependency("electricalprogressiveqol", "1.0.1")]
 [assembly: ModInfo(
     "Electrical Progressive: Equipment",
     "electricalprogressiveequipment",
     Website = "https://github.com/tehtelev/ElectricalProgressiveEquipment",
     Description = "Brings electricity into the game!",
-    Version = "1.0.0",
+    Version = "1.0.1",
     Authors = new[] {
         "Tehtelev",
         "Kotl"
